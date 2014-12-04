831eb637d57b71c5fcd5e33afac7bca235020c0b
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-12-04 10:30:49 +0100

    comit1

---
+++ src/probapaquete/Visualizar.java
@@ -0,0 +1,10 @@
+package probapaquete;
+
+import javax.swing.JOptionPane;
+
+public class Visualizar {
+    
+    public static void ver(){
+        JOptionPane.showMessageDialog(null, " vemos lo que Sergio dice");
+    }
+}
